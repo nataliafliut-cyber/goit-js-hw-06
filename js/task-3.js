ec2e45054c5f4f2eeb7f2de793b4bf11e12fff85
nataliafliut <nataliafliut@gmail.com>
2026-05-11 22:26:17 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,47 @@
+// Deklaracja poprawionej funkcji strzałkowej
+const sortByDescendingFriendCount = (users) => {
+  // b.friends.length - liczba przyjaciół drugiego użytkownika
+  // a.friends.length - liczba przyjaciół pierwszego użytkownika
+  return users.toSorted((a, b) => b.friends.length - a.friends.length);
+};
+
+// Kod do sprawdzenia
+console.log(
+  sortByDescendingFriendCount([
+    {
+      name: "Moore Hensley",
+      friends: ["Sharron Pace"],
+      gender: "male"
+    },
+    {
+      name: "Sharlene Bush",
+      friends: ["Briana Decker", "Sharron Pace"],
+      gender: "female"
+    },
+    {
+      name: "Ross Vazquez",
+      friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+      gender: "male"
+    },
+    {
+      name: "Elma Head",
+      friends: ["Goldie Gentry", "Aisha Tran"],
+      gender: "female"
+    },
+    {
+      name: "Carey Barr",
+      friends: ["Jordan Sampson", "Eddie Strong"],
+      gender: "male"
+    },
+    {
+      name: "Blackburn Dotson",
+      friends: ["Jacklyn Lucas", "Linda Chapman"],
+      gender: "male"
+    },
+    {
+      name: "Sheree Anthony",
+      friends: ["Goldie Gentry", "Briana Decker"],
+      gender: "female"
+    }
+  ])
+);
